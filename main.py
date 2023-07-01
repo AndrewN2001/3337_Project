89b15b7f8578c3a9dd9688df2cee7f628e4ad915
ANDREW NGUYEN <andrew.nguyen.ta@gmail.com>
2023-06-30 23:50:18 -0500

    imported needed sklearn modules

--- main.py
+++ main.py
@@ -2,16 +2,35 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pylab as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
 
 from matplotlib import rcParams
 rcParams['figure.figsize'] = 15, 5
 sns.set_style('darkgrid')
 
-df_90s = pd.read_csv('dataset-of-90s.csv')
-df_80s = pd.read_csv('dataset-of-80s.csv')
-df_70s = pd.read_csv('dataset-of-70s.csv')
 df_60s = pd.read_csv('dataset-of-60s.csv')
-df_10s = pd.read_csv('dataset-of-10s.csv')
-df_00s = pd.read_csv('dataset-of-00s.csv')
-print(df_00s.head())
-print(df_10s.info())
+print(df_60s.head())
+print(df_60s.info())
+
+df_60s.drop(labels=['track', 'artist', 'uri'], axis=1, inplace=True) # We do not need this as it will not affect our results.
+print(df_60s.info())
+
+# Data Cleanup
+# for track, group in df_60s.groupby('track'):
+#     if len(group) > 1:
+#         if group['target'].nunique() != 1 and group['artist'].nunique() != len(group):
+#             print(group)
+## Above is my attempt at cleaning up duplicate tracks that have different target values with the same artists.
+## None of the songs have them, so it's perfectly clean.
+
+print(df_60s.columns)
+
+sns.countplot(x=df_60s['target'])
+plt.show()
+
+
+
